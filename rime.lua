fd076f0b82cfe3878d75cb7858c41f6f783d815c
Dvel <git@dvel.me>
2022-09-29 20:20:56 +0800

    feat: iRime 支持

--- rime.lua
+++ rime.lua
@@ -207,3 +207,15 @@ function v_single_char_first_filter(input, seg)
     end
 end
 -------------------------------------------------------------
+-- iRime 九宫格专用，将输入框的数字转为对应的拼音或英文
+function irime_t9_preedit(input, env)
+	for cand in input:iter() do
+		if (string.find(cand.text, "%w+") ~= nil) then
+			cand:get_genuine().preedit = cand.text
+		else
+			cand:get_genuine().preedit = cand.comment
+		end
+		yield(cand)
+	 end
+end
+-------------------------------------------------------------
